86abc7267f8824216f08ecead7bca102be5abe6f
Antonio Seraus <41804476+SerausAntonio@users.noreply.github.com>
2023-10-22 19:26:03 +0200

    Second commit create user

--- src/test/java/day1/HTTPResquests.java
+++ src/test/java/day1/HTTPResquests.java
@@ -2,6 +2,8 @@ package day1;
 
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -29,5 +31,50 @@ public class HTTPResquests {
                 .statusCode(200)
                 .body("page",equalTo(2))
            .log().all();
+    }
+    @Test
+    public void userNotFound(){
+        given()
+          .when()
+           .get("https://reqres.in/api/users/23")
+            .then()
+           .statusCode(404);
+    }
+    @Test
+    public void listResource(){
+
+        given()
+                .when()
+                .get("https://reqres.in/api/unknown")
+                .then()
+                .statusCode(200)
+                .body("page",equalTo(1))
+                .log().all();
+    }
+    @Test
+    public void singleResource(){
+        given()
+                .when()
+                .get("https://reqres.in/api/unknown/2")
+                .then()
+                .statusCode(200)
+                .body("data.id",equalTo(2))
+                .log().all();
+    }
+
+    @Test
+    public void createUser(){
+        HashMap data = new HashMap();
+        data.put("name","pavan");
+        data.put("job","tester");
+
+        given()
+           .contentType("application/json")
+             .body(data)
+               .when()
+                  .post("https://reqres.in/api/users")
+               .then()
+                 .statusCode(201)
+                 .log().all();
     }
 }
